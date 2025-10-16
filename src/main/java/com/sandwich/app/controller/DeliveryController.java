a05151918cd71be10c57b734de5570a79e9ec88e
AnVoronov <AnVoronov@croc.ru>
2025-10-16 19:50:31 +0300

    Добавлены новые методы

--- src/main/java/com/sandwich/app/controller/DeliveryController.java
+++ src/main/java/com/sandwich/app/controller/DeliveryController.java
@@ -7,11 +7,15 @@ import com.sandwich.app.configuration.ProxyProperties;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.cloud.gateway.mvc.ProxyExchange;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @CrossOrigin
 @RestController
@@ -22,9 +26,21 @@ public class DeliveryController extends ProxyController {
         super(DELIVERY_PREFIX, properties.getDeliveryUrl(), request);
     }
 
-    @PreAuthorize("""
-        @jwtUtils.isSameUser(authentication, #id)
-        or hasAnyRole(T(com.sandwich.app.rbac.UserRole).ADMIN, T(com.sandwich.app.rbac.UserRole).MANAGER)""")
+    @PostMapping("/courier/create")
+    public ResponseEntity<byte[]> create(ProxyExchange<byte[]> proxy) {
+        return proxyPost(proxy);
+    }
+
+    @PostMapping("/courier/change-status/{id}")
+    public ResponseEntity<byte[]> changeStatus(@PathVariable UUID id, @RequestParam MultiValueMap<String, String> params, ProxyExchange<byte[]> proxy) {
+        return proxyPost(proxy);
+    }
+
+    @PostMapping("/delivery/change-status/{id}")
+    public ResponseEntity<byte[]> changeDeliveryStatus(@PathVariable UUID id, @RequestParam MultiValueMap<String, String> params, ProxyExchange<byte[]> proxy) {
+        return proxyPost(proxy);
+    }
+
     @PostMapping("/delivery/search")
     public ResponseEntity<byte[]> search(ProxyExchange<byte[]> proxy) {
         return proxyPost(proxy);
